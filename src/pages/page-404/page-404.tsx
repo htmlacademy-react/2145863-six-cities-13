a9e44f5f81dc5ee121798d9bd1025577af891f76
gitalexus <ru9.alex@gmail.com>
2023-07-14 10:07:42 +0300

    Refact page-404 to css-module styling

--- src/pages/page-404/page-404.tsx
+++ src/pages/page-404/page-404.tsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
-import './page-404.css';
 import { Helmet } from 'react-helmet-async';
+import css from './page-404.module.css';
+console.log(css);
 
 function Page404(): React.JSX.Element {
 	return (
-		<div className="container-error-404 error">
+		<div className={`${css.container} error`}>
 			<Helmet>
 				<title>6 Cities. 404 - Page not found</title>
 			</Helmet>
-			<img className="error__image" src="img/404.svg" width="252" height="294" alt="Специалист озадачен." />
-			<h1 className="error__title">404</h1>
-			<p className="error__desctription">Sorry, the page you visited does not exist.</p>
+			<img className={css.image} src="img/404.svg" width="252" height="294" alt="Специалист озадачен." />
+			<h1 className={css.title}>404</h1>
+			<p className={css.description}>Sorry, the page you visited does not exist.</p>
 			<Link to="/" className="button form__submit">Go to main</Link>
-			{/* <button className="button form__submit">Go to main</button> */}
 		</div>
 	);
 }
