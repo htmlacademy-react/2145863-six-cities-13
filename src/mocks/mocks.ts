bc66c64bfa53b3d5c69a4b30a9df94917aa19552
gitalexus <ru9.alex@gmail.com>
2023-07-16 11:04:55 +0300

    fix empty image array generation

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -68,7 +68,8 @@ function createFullMockOffer(mockOffer: ServerOffer): ServerFullOffer {
         isPro: faker.datatype.boolean(),
     },
     images: Array(faker.number.int({min: 0, max: 5}))
-							.map(() => faker.image.urlLoremFlickr({category: 'interior,room,modern,apartment'})),
+			.fill(null)
+			.map(() => faker.image.urlLoremFlickr({width: 260, height: 200, category: 'interior,room,modern,apartment'})),
     maxAdults: faker.number.int({min: 1, max: 5}),
 	}
 }
