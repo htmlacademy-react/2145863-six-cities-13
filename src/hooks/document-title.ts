d2d61229bebcf0dcf59b9ea05511133abb66640c
gitalexus <ru9.alex@gmail.com>
2023-07-21 18:13:27 +0300

    refact hook:useDocumentTitle

--- src/hooks/document-title.ts
+++ src/hooks/document-title.ts
@@ -2,13 +2,15 @@ import { useEffect } from 'react';
 import { PROJECT_NAME } from '../constants';
 
 export function useDocumentTitle(title: string) {
+
 	useEffect(() => {
 		const initialTitle = document.title;
-		document.title = ` ${PROJECT_NAME} · ${title}`;
-
 		return () => {
 			document.title = initialTitle;
 		};
+	}, []);
 
+	useEffect(() => {
+		document.title = ` ${PROJECT_NAME} · ${title}`;
 	}, [title]);
 }
