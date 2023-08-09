587a38ef47d3f2586b009118c9fd4b0348fdc476
gitalexus <ru9.alex@gmail.com>
2023-08-09 14:59:57 +0300

    Add: Api.ts (axios based object) and token.ts (using Local storage) modules

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+import type {AxiosInstance} from 'axios';
+import axios from 'axios';
+
+const BACKEND_URL = 'https://13.design.pages.academy';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+	const api = axios.create({
+		baseURL: BACKEND_URL,
+		timeout: REQUEST_TIMEOUT,
+	});
+
+	return api;
+};
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './root-reducer';
+import { createAPI } from '../services/api';
 
-export const store = configureStore({reducer: rootReducer});
+const api = createAPI();
+
+const store = configureStore({
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			thunk: {
+				extraArgument: api,
+			},
+		}),
+});
+
+export {api, store};
