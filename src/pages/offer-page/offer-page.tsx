21a108389eb4bcb89e424fa06eabc66f4d455eb3
gitalexus <ru9.alex@gmail.com>
2023-08-18 15:58:34 +0300

    Fix: after merge

--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -51,8 +51,6 @@ function OfferPage(): React.JSX.Element {
 		toast.error(`offer:${offerId} error ${ErrorCause.FetchOffer}`);
 	}
 
-	console.log('offer', offer);
-	console.log('description', offer.description);
 	return (
 		<div className="page">
 			<Header />
@@ -139,13 +137,13 @@ function OfferPage(): React.JSX.Element {
 
 							</div>
 						</div>
-						{/* <LeafletMap
+						<LeafletMap
 							block="offer"
 							neighborhoodOffers={neighbourPlaces}
-						/> */}
+						/>
 					</section>
 					<div className="container">
-						{/* {neighbourPlaces &&
+						{neighbourPlaces &&
 							<section className="near-places places">
 								<h2 className="near-places__title">
 									Other places in the neighbourhood
@@ -156,200 +154,13 @@ function OfferPage(): React.JSX.Element {
 									)}
 
 								</div>
-							</section>} */}
+							</section>}
 					</div>
 				</main>
 			)}
 
 		</div>
-	)
+	);
 }
-
-
-
-	// useEffect(() => {
-		// 	if (offerId) {
-			// 		// dispatch(offersActions.fetchOffer(offerId));
-	// 		// dispatch(offersActions.fetchReviews(offerId));
-	// 		// dispatch(offersActions.fetchNeighborPlaces(offerId));
-	// 	}
-
-	// 	// return () => {
-		// 	// 	dispatch(offersActions.dropOffer());
-	// 	// };
-	// }, [offerId, dispatch]); // зачем dispatch
-
-	// if (isEmpty)
-	// return (
-	// 	<>
-	// 		<LoadingScreen />
-	// 	</>
-	// );
-
-	// const favoriteLabel = `${offer?.isFavorite ? 'In' : 'To'} bookmarks`;
-	// const bookmarkClass = clsx(
-	// 	'offer__bookmark-button',
-	// 	offer?.isFavorite && 'offer__bookmark-button--active',
-	// 	'button');
-	// const hostAvatarClass = clsx(
-	// 	'offer__avatar-wrapper',
-	// 	offer?.host.isPro && 'offer__avatar-wrapper--pro',
-	// 	'user__avatar-wrapper');
-	// const isAuthorized = AuthorizationStatus === AuthorizationStatus.Auth;
-
-
-	// return (
-	// 	<div className="page">
-	// 		<Header isAuthorized={isAuthorized} />
-	// 		{offer === null &&
-	// 			<Page404 />}
-	// 		{offer &&
-	// 			<main className="page__main page__main--offer">
-	// 				<section className="offer">
-
-	// 					<div className="offer__gallery-container container">
-	// 						<div className="offer__gallery">
-	// 							{offer.images.map((image) =>
-	// 								<GalleryImage imageSrc={image} key={image + offer.id} />)}
-	// 						</div>
-	// 					</div>
-
-	// 					<div className="offer__container container">
-	// 						<div className="offer__wrapper">
-	// 							{offer.isPremium && <div className="offer__mark"><span>Premium</span></div>}
-	// 							<div className="offer__name-wrapper">
-	// 								<h1 className="offer__name">
-	// 									{offer.title}
-	// 								</h1>
-	// 								<button className={bookmarkClass} type="button">
-	// 									<svg className="offer__bookmark-icon" width={31} height={33}>
-	// 										<use xlinkHref="#icon-bookmark" />
-	// 									</svg>
-	// 									<span className="visually-hidden">{favoriteLabel}</span>
-	// 								</button>
-	// 							</div>
-
-	// 							<div className="offer__rating rating">
-	// 								<div className="offer__stars rating__stars">
-	// 									<span style={{ width: `${offer.rating * 20}%` }} />
-	// 									<span className="visually-hidden">Rating</span>
-	// 								</div>
-	// 								<span className="offer__rating-value rating__value">{offer.rating}</span>
-	// 							</div>
-	// 							<ul className="offer__features">
-	// 								<li className="offer__feature offer__feature--entire">{offer.type}</li>
-	// 								<li className="offer__feature offer__feature--bedrooms">
-	// 									{offer.bedrooms} Bedrooms
-	// 								</li>
-	// 								<li className="offer__feature offer__feature--adults">
-	// 									Max {offer.maxAdults} adults
-	// 								</li>
-	// 							</ul>
-	// 							<div className="offer__price">
-	// 								<b className="offer__price-value">€{offer.price}</b>
-	// 								<span className="offer__price-text">&nbsp;night</span>
-	// 							</div>
-
-	// 							<div className="offer__inside">
-	// 								<h2 className="offer__inside-title">What&apos;s inside</h2>
-	// 								<ul className="offer__inside-list">
-	// 									{offer.goods.map((good, index) =>
-	// 										<li className="offer__inside-item" key={index.toString() + crypto.randomUUID()}>{good}</li>
-	// 									)}
-	// 								</ul>
-	// 							</div>
-
-	// 							<div className="offer__host">
-	// 								<h2 className="offer__host-title">Meet the host</h2>
-	// 								<div className="offer__host-user user">
-	// 									<div className={hostAvatarClass}>
-	// 										<img
-	// 											className="offer__avatar user__avatar"
-	// 											src={offer.host.avatarUrl}
-	// 											width={74}
-	// 											height={74}
-	// 											alt="Host avatar"
-	// 										/>
-	// 									</div>
-	// 									<span className="offer__user-name">{offer.host.name}</span>
-	// 									{offer.host.isPro && <span className="offer__user-status">Pro</span>}
-	// 								</div>
-	// 								{/* description */}
-	// 								<div className="offer__description">
-	// 									{offer.description.map((desc, index) =>
-	// 										(
-	// 											<p className="offer__text" key={index.toString() + crypto.randomUUID()}>{desc}</p>
-	// 										)
-	// 									)}
-	// 								</div>
-	// 							</div>
-
-	// 							{/* reviews */}
-	// 							<section className="offer__reviews reviews">
-	// 								<h2 className="reviews__title">
-	// 									Reviews
-	// 									{reviews?.length > 0 &&
-	// 										<> · <span className="reviews__amount">{reviews.length}</span></>}
-	// 								</h2>
-	// 								<ul className="reviews__list">
-	// 									{reviews?.map((review) => (
-	// 										<li className="reviews__item" key={review.id}>
-	// 											<div className="reviews__user user">
-	// 												<div className="reviews__avatar-wrapper user__avatar-wrapper">
-	// 													<img
-	// 														className="reviews__avatar user__avatar"
-	// 														src={review.user.avatarUrl}
-	// 														width={54}
-	// 														height={54}
-	// 														alt="Reviews avatar"
-	// 													/>
-	// 												</div>
-	// 												<span className="reviews__user-name">{review.user.name}</span>
-	// 											</div>
-	// 											<div className="reviews__info">
-	// 												<div className="reviews__rating rating">
-	// 													<div className="reviews__stars rating__stars">
-	// 														<span style={{ width: `${review.rating * 20}%` }} />
-	// 														<span className="visually-hidden">Rating</span>
-	// 													</div>
-	// 												</div>
-	// 												<p className="reviews__text">
-	// 													{review.comment}
-	// 												</p>
-	// 												<time className="reviews__time" dateTime={getReviewDateTime(review.date)}>
-	// 													{getReviewDateString(review.date)}
-	// 												</time>
-	// 											</div>
-	// 										</li>
-	// 									))}
-
-	// 								</ul>
-	// 								{isAuthorized && <NewCommentForm />}
-	// 							</section>
-	// 						</div>
-	// 					</div>
-	// 					<LeafletMap
-	// 						block="offer"
-	// 						neighborhoodOffers={neighbourPlaces}
-	// 					/>
-	// 				</section>
-	// 				<div className="container">
-	// 					{neighbourPlaces &&
-	// 						<section className="near-places places">
-	// 							<h2 className="near-places__title">
-	// 								Other places in the neighbourhood
-	// 							</h2>
-	// 							<div className="near-places__list places__list">
-	// 								{neighbourPlaces.map((place) =>
-	// 									<Card block='near-places' offer={place} key={place.id} />
-	// 								)}
-
-	// 							</div>
-	// 						</section>}
-	// 				</div>
-	// 			</main>}
-	// 	</div>
-	// );
-// }
 
 export default OfferPage;
